6710e9d63ad474b505479c1a08e2df413e364486
M.Ali Shakeel <alishakeel567567@gmail.com>
2025-01-24 11:58:30 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Sidemenu from "./Component/menu";
+import "./App.css";
+
+function App() {
+  return <>
+  <Sidemenu />
+  </>;
+}
+
+export default App;
