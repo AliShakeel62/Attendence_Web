efbb472ba00f0238f6b3d350f92525d58bb2f46f
M.Ali Shakeel <alishakeel567567@gmail.com>
2025-01-31 19:28:58 +0500

    first commit

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
 
 const Model: React.FC = () => {
+  const [Class,setClass] = useState('');
+  const [Teacher,setTeacher] = useState('');
+  const [limit,setlimit] = useState('');
+  
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
@@ -16,18 +20,24 @@ const Model: React.FC = () => {
     setIsModalOpen(false);
   };
 
+
   return (
     <>
       <button className='btn' onClick={showModal}>
         Edit
       </button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{ style: { backgroundColor: 'whitesmoke', color: 'black' ,border:"1px solid" ,borderColor:"whitesmoke" } }} // Custom color
+      >
         <div className='d-flex flex-column '>
-        <input type="text" placeholder='Update ClassName' className='inp m-1' />
-       <input type="text" placeholder='Update TeacherName' className='inp m-1' />
-       <input type="text" placeholder='Update Student Limit' className='inp m-1'  />
+          <input type="text" placeholder='Update ClassName' className='inp m-1' />
+          <input type="text" placeholder='Update TeacherName' className='inp m-1' />
+          <input type="text" placeholder='Update Student Limit' className='inp m-1' />
         </div>
-
       </Modal>
     </>
   );
